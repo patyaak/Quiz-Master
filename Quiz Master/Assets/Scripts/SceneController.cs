68feaada16692eafbd14c79fd506ef7a91a8e8bf
Pratik Mulguthi <98331572+patyaak@users.noreply.github.com>
2024-09-21 23:41:24 +0545

    quiz

---
+++ Quiz Master/Assets/Scripts/SceneController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    
+
+    public void QuitGame()
+    {
+       
+#if UNITY_STANDALONE
+        Application.Quit();
+#endif
+    }
+}
